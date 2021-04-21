7df5dffb1cb8c1ce7cfcba2ad45e20ad641bfce2
Minh Trần <tranminhbvhp@gmail.com>
2021-04-21 16:53:09 +0700

    move dll files to lib folder

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -26,7 +26,7 @@ namespace BVCareManager.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Data\\BVCareMa" +
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\data\\BVCareMa" +
             "nager.mdf;Integrated Security=True;Connect Timeout=10;Packet Size=1000")]
         public string BVCareManagerConnectionString {
             get {
