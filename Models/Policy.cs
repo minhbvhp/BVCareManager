d7f368ca24564405b089c8d5e89e413211c06d11
Minh Trần <tranminhbvhp@gmail.com>
2021-04-28 16:53:40 +0700

    Premium of Policy update

--- Models/Policy.cs
+++ Models/Policy.cs
@@ -8,6 +8,33 @@ namespace BVCareManager.Models
 {
     public partial class Policy
     {
-        public int Premium { get; set; }
+        public int Premium {
+            get
+            {
+                int monthsCover = (this.ToDate.Year - this.FromDate.Year) * 12 + this.ToDate.Month - this.FromDate.Month;
+                int _premium = 0;
+                int _premiumOfContract = this.Contract.AnnualPremiumPerInsured;
+
+                if (monthsCover > 0 && monthsCover <= 3)
+                {
+                    _premium = _premiumOfContract * 30 / 100;
+                } 
+                else if (monthsCover > 3 && monthsCover <= 6)
+                {
+                    _premium = _premiumOfContract * 60 / 100;
+                }
+                else if (monthsCover > 6 && monthsCover <= 9)
+                {
+                    _premium = _premiumOfContract * 85 / 100;
+                }
+                else
+                {
+                    _premium = _premiumOfContract;
+                }
+
+                return _premium;
+            }
+            
+        }
     }
 }
